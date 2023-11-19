883c0e335039b50a0c791014db43b85a25904b5a
agungricky07 <agungricky07@gmail.com>
2023-11-19 19:44:33 +0700

    Upload Original

---
+++ Api_mobile/routes/api.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Http\Controllers\ApiController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::get('/viewText', function () {
+    dd('Selamat datang');
+});
+
+Route::get('alldata', [ApiController::class, 'index']);
